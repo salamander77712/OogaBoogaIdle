c22e347a8a17fdcf6dc5ed5e6313d6db2c724233
PickleSalamander <xander.h.wright@gmail.com>
2023-07-14 19:57:52 -0400

    Made child scene for resource labels

---
+++ ResourceLabel.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class ResourceLabel : Label
+{
+    [Export]
+    float resourceValue;
+
+    [Export]
+    string resourceName;
+    public override void _Ready()
+    {
+        updateLabel(resourceValue);
+    }
+
+    public void updateLabel(float newResouceValue){
+        resourceValue = newResouceValue;
+        Text = resourceName + ": " + resourceValue;
+    }
+}
